1ae7109d07bee36b81278f08fa10e8939a0d8be9
Ravi Jha <ravi.jha@astronomer.io>
2026-04-20 15:43:01 -0500

    Remove dim table dependencies from fact_orders to work within X-Small warehouse constraints

--- dbt/models/marts/fact_orders.sql
+++ dbt/models/marts/fact_orders.sql
@@ -2,19 +2,13 @@ with orders as (
 
     select * from {{ ref('int_orders_enriched') }}
 
-),
-
-dim_customers as (
-
-    select customer_id, customer_key from {{ ref('dim_customers') }}
-
 )
 
 select
-    {{ dbt_utils.generate_surrogate_key(['o.order_id']) }}  as order_key,
+    {{ dbt_utils.generate_surrogate_key(['o.order_id']) }}       as order_key,
     o.order_id,
     o.customer_id,
-    c.customer_key,
+    {{ dbt_utils.generate_surrogate_key(['o.customer_id']) }}    as customer_key,
     o.region,
     o.order_date,
     o.order_hour,
@@ -24,8 +18,6 @@ select
     o.has_promo,
     o.total_amount,
     o.continent,
-    o.customer_tier                                          as tier
+    o.customer_tier                                              as tier
 
 from orders o
-left join dim_customers c
-    on o.customer_id = c.customer_id
